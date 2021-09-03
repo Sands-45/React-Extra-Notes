f96ff411b0cd73cc7debabff217e28d010c0057b
Sands <82897602+Sands-45@users.noreply.github.com>
2021-09-03 12:25:26 +0200

    Update react.js

--- react.js
+++ react.js
@@ -289,3 +289,27 @@ class Controller extends React.Component {
     );
   }
 }
+
+
+//Inline Styling 
+const styles={
+  color: "purple",
+  fontSize: 40,
+  border: "2px solid purple"
+}
+class Colorful extends React.Component {
+  render() {
+    return (
+      <div style={styles}>Style Me!</div>
+    );
+  }
+};
+
+//Or
+class Colorful extends React.Component {
+  render() {
+    return (
+      <div style={{color:"purple",border: "solid 2px purple"}>Style Me!</div>
+    );
+  }
+};
