7632fef99f0cc0ecb91db3ef5b27ec630ed776e8
Sands <82897602+Sands-45@users.noreply.github.com>
2021-09-03 15:03:19 +0200

    Update react.js

--- react.js
+++ react.js
@@ -471,3 +471,51 @@ function Frameworks() {
     </div>
   );
 };
+
+
+//Using filter() & map() array methods
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [
+        {
+          username: 'Jeff',
+          online: true
+        },
+        {
+          username: 'Alan',
+          online: false
+        },
+        {
+          username: 'Mary',
+          online: true
+        },
+        {
+          username: 'Jim',
+          online: false
+        },
+        {
+          username: 'Sara',
+          online: true
+        },
+        {
+          username: 'Laura',
+          online: true
+        }
+      ]
+    };
+  }
+  render() {
+    const usersOnline = this.state.users.filter(user => user.online === true)
+    const renderOnline = usersOnline.map((user,index)=>
+      <li key={index}>{user.username}</li>
+    )
+    return (
+      <div>
+        <h1>Current Online Users:</h1>
+        <ul>{renderOnline}</ul>
+      </div>
+    );
+  }
+}
