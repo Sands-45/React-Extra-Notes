41e7dfdbbba391ac9a7acef027b0c841033d9661
Sands <82897602+Sands-45@users.noreply.github.com>
2021-09-03 12:14:31 +0200

    Update react.js

--- react.js
+++ react.js
@@ -242,3 +242,50 @@ document.removeEventListener("keydown",this.handleKeyPress)
     );
   }
 };
+
+class OnlyEvens extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('Should I update?');
+    // Change code below this line
+    if(nextProps.value % 2 === 0){
+      return true
+    }else{
+      return false
+    }
+    // Change code above this line
+  }
+  componentDidUpdate() {
+    console.log('Component re-rendered.');
+  }
+  render() {
+    return <h1>{this.props.value}</h1>;
+  }
+}
+
+
+//Optimize Re-Renders with shouldComponentUpdate
+class Controller extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0
+    };
+    this.addValue = this.addValue.bind(this);
+  }
+  addValue() {
+    this.setState(state => ({
+      value: state.value + 1
+    }));
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.addValue}>Add</button>
+        <OnlyEvens value={this.state.value} />
+      </div>
+    );
+  }
+}
