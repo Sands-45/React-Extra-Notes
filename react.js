894a47b7c20bffaeee6cd96f0fc23c84651ce3e5
Sands <82897602+Sands-45@users.noreply.github.com>
2021-09-03 10:54:08 +0200

    Update react.js

--- react.js
+++ react.js
@@ -179,3 +179,29 @@ class MyForm extends React.Component {
     );
   }
 }
+
+//Use the Lifecycle Method componentDidMount
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeUsers: null
+    };
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        activeUsers: 1273
+      });
+    }, 2500);
+  }
+  render() {
+    return (
+      <div>
+        {/* Change code below this line */}
+        <h1>Active Users:{this.state.activeUsers} </h1>
+        {/* Change code above this line */}
+      </div>
+    );
+  }
+}
