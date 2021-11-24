4ac31276eabba001a62cbc27f0ec0b57e9ddc1a7
Sands <82897602+Sands-45@users.noreply.github.com>
2021-11-24 23:46:16 +0200

    Update redux.js

--- redux.js
+++ redux.js
@@ -17,3 +17,37 @@ const loginAction = () => {
     type: 'LOGIN'
   }
 };
+
+// using Switch statement to conditionally update the state in Redux 
+const defaultState = {
+  authenticated: false
+};
+
+const authReducer = (state = defaultState, action) => {
+  // Change code below this line
+  switch(action.type){
+    case "LOGIN":
+    return {authenticated:true}
+    break;
+    case "LOGOUT":
+    return {authenticated:false}
+    break;
+    default:
+    return {authenticated:false}
+  }
+  // Change code above this line
+};
+
+const store = Redux.createStore(authReducer);
+
+const loginUser = () => {
+  return {
+    type: 'LOGIN'
+  }
+};
+
+const logoutUser = () => {
+  return {
+    type: 'LOGOUT'
+  }
+};
