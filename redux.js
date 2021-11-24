00114e4328d6bd48764d0abd7fe27d23167eccab
Sands <82897602+Sands-45@users.noreply.github.com>
2021-11-24 23:23:22 +0200

    Update and rename README.md to redux.js

---
+++ redux.js
@@ -0,0 +1,19 @@
+# React-Extra-Notes
+//Redux
+const defaultState = {
+  login: false
+};
+
+const reducer = (state = defaultState, action) => {
+  // Change code below this line
+  return action.type === "LOGIN"?{...state,login:true}:state
+  // Change code above this line
+};
+
+const store = Redux.createStore(reducer);
+
+const loginAction = () => {
+  return {
+    type: 'LOGIN'
+  }
+};
